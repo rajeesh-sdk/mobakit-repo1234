1099722d6ca32b63673bcb40449548c68c5a2dc3
Rajeesh Koroth <rkoroth@gmail.com>
2022-09-21 17:37:31 +0530

    test script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -e
-cd /home/ec2-user/mobakit-repo123
-npm install
-npm install pm2 -g
-pm2 start API/index.js --name one
+service nginx restart
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 yum update -y
-
+cd /home/ec2-user/mobakit-repo/
+rm -rf *
+sudo cp -r ./dist/ /usr/share/nginx/html/
